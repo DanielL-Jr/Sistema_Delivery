d73838fe38e2963e3c2b5506a1c1876b64ff2b24
DanielL-Jr <136136240+DanielL-Jr@users.noreply.github.com>
2023-06-11 10:02:31 -0300

    Adição do bd

---
+++ backend/prisma/migrations/20230611130101_create_all_tables/migration.sql
@@ -0,0 +1,41 @@
+-- CreateTable
+CREATE TABLE "clientes" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "cpf" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "telefone" TEXT NOT NULL,
+    "cep" TEXT NOT NULL,
+    "estado" TEXT NOT NULL,
+    "cidade" TEXT NOT NULL,
+    "bairro" TEXT NOT NULL,
+    "endereco" TEXT NOT NULL,
+    "numero" TEXT NOT NULL,
+    "complemento" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "comidas" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "nome" TEXT NOT NULL,
+    "imagem" TEXT NOT NULL,
+    "valor" DECIMAL NOT NULL,
+    "descricao" TEXT NOT NULL,
+    "tipo" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "pedidos" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id_cli" INTEGER NOT NULL,
+    "id_food" INTEGER NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "pedidos_id_food_fkey" FOREIGN KEY ("id_food") REFERENCES "comidas" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "pedidos_id_cli_fkey" FOREIGN KEY ("id_cli") REFERENCES "clientes" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "clientes_cpf_key" ON "clientes"("cpf");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "clientes_email_key" ON "clientes"("email");
